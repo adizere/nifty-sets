18299f9584a70069ceeea1fb1624eab6e60ef51f
Dragos-Adrian Seredinschi <dragos-adrian.seredinschi@epfl.ch>
2015-01-18 21:45:54 +0100

    Basic query and construction done.

--- src/Data/PackedSet.hs
+++ src/Data/PackedSet.hs
@@ -1,18 +1,126 @@
 module Data.PackedSet where
 
-
-import qualified Data.Set   as S
+import Data.Maybe           (maybe)
+import qualified Data.Set   as ST
+import qualified Data.Map   as MP
 
 
 data PackedSet a = PackedSet
-    { psSize        :: Int
-    , psLowerLimit  :: a
-    , upperLimit    :: a
-    , outliers      :: S.Set (a)
-    }
+       -- Total number of elements.
+    { psSize            :: Int
+        -- Main interval limits.
+    , psMainInterval    :: (a,a)
+        -- Lower end of the intervals, speeds-up lookups.
+    , psIntervalsLE     :: ST.Set a
+        -- All the outlying interval; we assume at some point the main interval
+        -- will swallow up all these.
+    , psIntervals       :: MP.Map a (a,a)
+        -- Outliers are not part of either the main interval or the other
+        -- intervals, so keep them in a separate set. We assume eventually
+        -- these will merge in
+    , psOutliers        :: ST.Set a
+    } deriving (Show)
 
 
--- instance Show PackedSet where
---     show PackedSet {upperLimit = u, outliers = o} =
---         "DG: {" ++ show u ++ ", " ++ show (S.size o) ++ "}"
+{--------------------------------------------------------------------
+  Construction
+--------------------------------------------------------------------}
+-- | /O(1)/. The empty packet set.
+empty :: PackedSet Int
+empty =
+    PackedSet { psSize          = 0
+              , psMainInterval  = (0,0)
+              , psIntervalsLE   = ST.empty
+              , psIntervals     = MP.empty
+              , psOutliers      = ST.empty
+              }
 
+
+-- | /O(1)/. Create a singleton packet set.
+singleton :: Ord a => a -> PackedSet a
+singleton x =
+    PackedSet { psSize          = 1
+              , psMainInterval  = (x,x)
+              , psIntervalsLE   = ST.empty
+              , psIntervals     = MP.empty
+              , psOutliers      = ST.empty
+              }
+
+
+{--------------------------------------------------------------------
+  Query
+--------------------------------------------------------------------}
+-- | /O(1)/. Is this packed set empty?
+null :: PackedSet a -> Bool
+null PackedSet { psSize = s } =
+    s == 0
+
+
+-- | /O(1)/. Returns the number of elements in the packed set.
+size :: PackedSet a -> Int
+size PackedSet {psSize = s} =
+    s
+
+
+-- | /O(log n)/. Lookup a member in the packed set.
+-- TODO!
+member :: Ord a => a -> PackedSet a -> Bool
+member m p =
+    if (psSize p == 0)
+        then False
+        else if (_belongsTo m mi) || (ST.member m ol)
+                    then True
+                    else _intervalsLookup m p
+    where
+        ol = psOutliers p
+        mi = psMainInterval p
+
+-- | /O(log n)/. Is the element not in the packed set?
+notMember :: Ord a => a -> PackedSet a -> Bool
+notMember m p = not $ member m p
+
+
+{--------------------------------------------------------------------
+  Insertion
+--------------------------------------------------------------------}
+-- | /O(log n)/. Insert an element in a packed set if it's not a member already.
+-- insert :: Ord a => a -> PackedSet a -> PackedSet a
+-- insert m p =
+--     if (psSize p) == 0
+--         then singleton m
+--         else if condition
+--             then expression
+--             else expression
+--             -- if (m == psll - 1)
+--             --     then newPSULimit
+--             --     else if (m == psll - 1)
+--             --         then expression
+--             --         else expression
+--     where
+--         (psll, psul) = psMainInterval p
+--     --     pso = psOutliers p
+--         newPSULimit = p { psMainInterval = (m, psul) }
+
+
+
+
+{--------------------------------------------------------------------
+  Private functions
+--------------------------------------------------------------------}
+-- Looks-up an element in all the outlying intervals of a packed set.
+_intervalsLookup :: Ord a => a -> PackedSet a -> Bool
+_intervalsLookup m p =
+    maybe False
+          (\se -> maybe False validateInterval $ mInterval se)
+          mIntervalSE
+    where
+        -- find the interval lower end, if any
+        mIntervalSE = ST.lookupLE m (psIntervalsLE p)
+        mInterval k = MP.lookup k (psIntervals p)
+        validateInterval (_, iGE) = iGE > m
+
+
+-- Returns true if an element is inside an interval.
+_belongsTo :: Ord a => a -> (a,a) -> Bool
+_belongsTo m (lowerLim, upperLim) =
+    m >= lowerLim && m <= upperLim
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -5,16 +5,28 @@ import Test.Hspec
 import qualified Data.PackedSet as PS
 
 
+mySpecs :: Spec
 mySpecs =
-    describe "Verify that bassbull outputs the correct data" $ do
-        it "equals zero" $ do
-            let dummy = 10
-            dummy `shouldBe` 10
-    -- describe "PackedSet creation"
-    -- [
-    --     it "bla"
-    --            (pending "reason..")
-    -- ]
+    describe "Data.PackedSet construction" $ do
+        let p = PS.empty :: PS.PackedSet Int
+        it "constructs an empty packet set" $ do
+            PS.null p `shouldBe` True
+        it "tests the size of an empty set" $ do
+            PS.size p `shouldBe` 0
+        it "tests the `member` function on an empty set" $ do
+            PS.member 0 p `shouldBe` False
+        it "tests the `notMember` function on an empty set" $ do
+            PS.notMember 0 p `shouldBe` True
+
+        let p = PS.singleton 888 :: PS.PackedSet Int
+        it "constructs a singleton set" $ do
+            PS.null p `shouldBe` False
+            PS.size p `shouldBe` 1
+            PS.member 0 p `shouldBe` False
+            PS.member 888 p `shouldBe` True
+            PS.notMember 0 p `shouldBe` True
+            PS.notMember 888 p `shouldBe` False
+
 
 main :: IO ()
 main = hspec mySpecs
